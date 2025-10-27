62c7a2d563c69060b72657e5edaaa59d537cda48
Jamie Doyle <2003jay04@gmail.com>
2025-10-27 17:37:40 +0000

    746

---
+++ leetcode/746.py
@@ -0,0 +1,8 @@
+def minCostClimbingStairs(cost: list[int]):
+
+    cost.append(0)
+
+    for i in range(len(cost)-3, -1, -1):
+        cost[i] = min(cost[i + 1], cost[i + 2])
+
+    return min(cost[0], cost[1])
