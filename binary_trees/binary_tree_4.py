2deb06c3c4770515e7d19845d5df2e8e828e9164
Jamie Doyle <2003jay04@gmail.com>
2025-11-07 20:43:11 +0000

    Trees, traversal, iterative and recursively

---
+++ binary_trees/binary_tree_4.py
@@ -0,0 +1,73 @@
+from collections import deque
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+
+def preorder(root: Node):
+    if not root: return []
+    return [root.val] + preorder(root.left) + preorder(root.left)
+
+
+def inorder(root: Node):
+    if not root: return []
+    return inorder(root.left) + [root.val] + inorder(root.left)
+
+
+def postorder(root: Node):
+    if not root: return []
+    return  postorder(root.left) + postorder(root.left) + [root.val]
+
+def inorder_iter(root: Node):
+    if not root:
+        return []
+    
+    stack = [root]
+    inorder = []
+    while stack:
+        node = stack.pop()
+        inorder.append(node.val)
+        if node.left:
+            inorder.append(node.left)
+        if node.right:
+            inorder.append(node.right)
+
+    return inorder
+
+def BFS(root: Node):
+    q = deque([root])
+    bfs = []
+
+    while q:
+        node = q.popleft()
+        bfs.append(node.val)
+
+        if node.left:
+            q.append(node.left)
+
+        if node.right:
+            q.append(node.right)
+    return bfs
+
+#       A
+#     B   C
+#   D  E    F
+
+print(BFS(a))
+print(inorder_iter(a))
